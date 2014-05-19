e570f716a695da8117057e24e765252586c120c0
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-05-19 13:39:00 +0200

    Add field email

---
+++ OctopusDeploy.QFrameBootcamp.Begin/OctopusDeploy.QFrameBootcamp.DB/M003_User_Email.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace OctopusDeploy.QFrameBootcamp.DB
+{
+    [Migration(003)]
+    public class M003_User_Email : Migration
+    {
+        public override void Up()
+        {
+            Create
+                .Column("Email")
+                .OnTable("Users")
+                .AsString(75)
+                .Nullable();
+
+            Execute.Sql("UPDATE Users SET Email = FirstName + '.' + LastName + '@qframe.be'");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Email")
+                  .FromTable("Users");
+        }
+    }
+}
